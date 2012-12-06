251152c689ce67b5fd0261dd01f619f527cf6716
Aditya Singh <aditya@impulsemedia.co.in>
2012-12-07 04:55:07 +0530

    modified:   bidder.py

--- bidder.py
+++ bidder.py
@@ -187,6 +187,8 @@ class MainHandler(tornado.web.RequestHandler):
 		response.processing_time_ms=int((time.time()-start)*1000)
 		
 	    bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"] += 1
+	    compare = (time.time() - bidCountIndex["GoogleAdX"][domain]["DisplayWeb"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Lastupdate"])
+	    print "compare = "+str(compare)
 	    if (time.time() - bidCountIndex["GoogleAdX"][domain]["DisplayWeb"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Lastupdate"])>10:
 	      i = bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"]
 	      message = json.dumps({"messageType":"Forecast", "message":{"e":"GoogleAdX", "d":domain , "c":"DesktopDisplay" ,"geo":country.upper(),
