32c5c451163c699519803e10848fdb94c628a77c
Aditya Singh <aditya@impulsemedia.co.in>
2012-12-07 06:36:54 +0530

    modified:   bidder.py

--- bidder.py
+++ bidder.py
@@ -39,172 +39,178 @@ class MainHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     @tornado.gen.engine
     def post(self):
-	global geoIndex
-	global campaignData
-	global ruleSet
-	global con
-	global cur
-	global india_tz
-	global india_time
-	global bidCountIndex
-        start = time.time()
-        postContent = self.request.body
-        bidRequest = realtime_bidding_proto_pb2.BidRequest()
-        try:
-	    bidRequest.ParseFromString(postContent)
-            domain = re.sub('www.',r'',str(urlparse(bidRequest.url).netloc))
+        if self.request.path == "/":
+	    self.write("Nothing for u here. Go <a href='http://google.com'>here</a>")
+      
+        if self.request.path == "/getbid":
+	    global geoIndex
+	    global campaignData
+	    global ruleSet
+	    global con
+	    global cur
+	    global india_tz
+	    global india_time
+	    global bidCountIndex
+	    start = time.time()
+	    postContent = self.request.body
+	    bidRequest = realtime_bidding_proto_pb2.BidRequest()
+	    try:
+		bidRequest.ParseFromString(postContent)
+		domain = re.sub('www.',r'',str(urlparse(bidRequest.url).netloc))
 
-            geo_criteria_id=bidRequest.geo_criteria_id
-	    if str(geo_criteria_id) in geoIndex.keys():
-		country = geoIndex[str(geo_criteria_id)]["Country"].lower()
-		cType= geoIndex[str(geo_criteria_id)]["Type"]
-		if cType=="City":
-		    city=geoIndex[str(geo_criteria_id)]["Name"].lower()
-		    state=geoIndex[geoIndex[str(geo_criteria_id)]["Parent"]]["Name"].lower()
-		if cType=="State":
-		    state=geoIndex[str(geo_criteria_id)]["Name"].lower()
-		    city=""
-		if cType=="UT":
-		    state=geoIndex[str(geo_criteria_id)]["Name"].lower()
-		    city=""
+		geo_criteria_id=bidRequest.geo_criteria_id
+		if str(geo_criteria_id) in geoIndex.keys():
+		    country = geoIndex[str(geo_criteria_id)]["Country"].lower()
+		    cType= geoIndex[str(geo_criteria_id)]["Type"]
+		    if cType=="City":
+			city=geoIndex[str(geo_criteria_id)]["Name"].lower()
+			state=geoIndex[geoIndex[str(geo_criteria_id)]["Parent"]]["Name"].lower()
+		    if cType=="State":
+			state=geoIndex[str(geo_criteria_id)]["Name"].lower()
+			city=""
+		    if cType=="UT":
+			state=geoIndex[str(geo_criteria_id)]["Name"].lower()
+			city=""
 
-	    if not bidRequest.HasField("anonymous_id") and not bidRequest.HasField("mobile") and not bidRequest.HasField("video") and not bidRequest.is_ping:
-		#segments = yield tornado.gen.Task(redisClient.smembers,'user:'+bidRequest.google_user_id)
-		segments=[]
-		adSlots = bidRequest.adslot
-		response = realtime_bidding_proto_pb2.BidResponse()
+		if not bidRequest.HasField("anonymous_id") and not bidRequest.HasField("mobile") and not bidRequest.HasField("video") and not bidRequest.is_ping:
+		    #segments = yield tornado.gen.Task(redisClient.smembers,'user:'+bidRequest.google_user_id)
+		    segments=[]
+		    adSlots = bidRequest.adslot
+		    response = realtime_bidding_proto_pb2.BidResponse()
 
-		for ad in adSlots:
-		    if segments:
-			for seg in segments:
-			    try:
-				audienceCampaigns = campaignData['display:audience:' + str(seg)]
-			    except KeyError:
-				audienceCampaigns=list()
-		    else:
-			audienceCampaigns=list()
-
-		    try:
-			ronCampaigns = campaignData['display:roe']
-		    except KeyError:
-			ronCampaigns = list()
-
-		    try:
-			black = campaignData['display:roe:black:'+domain]		#ROE Campaigns with this domain as blacklist
-		    except KeyError:
-			black = list()
-
-		    ronCampaigns = list(set(ronCampaigns) - set(black))			
-
-		    try:
-		        whiteCampaigns = campaignData['display:white:'+domain]
-		    except KeyError:
-			whiteCampaigns = list()
-
-		    campaigns = list(set(audienceCampaigns+ronCampaigns+whiteCampaigns))
-		    
-		    geoCampaigns = campaignData['display:geo:'+country]
-		    campaigns = list(set(geoCampaigns) & set(campaigns))
-		    
-		    size=str(ad.width[0])+"x"+str(ad.height[0])
-		    try:
-		        sizeCampaigns = campaignData['display:size:'+size]
-		    except KeyError:
-			sizeCampaigns = list()
-			
-		    campaigns = list(set(sizeCampaigns) & set(campaigns))
-		    
-		    if(len(campaigns)>0):
-			camplist=[]
-			for camp in campaigns:
-			    l = [camp, campaignData["display:campaign:"+str(camp)+":bid"],campaignData["display:campaign:"+str(camp)+":pacing"]]
-			    camplist.append(l)
-			camplist.sort(key=operator.itemgetter(1), reverse=True) # sorts the list in place decending by bids
-
-			#Retrieve rules from SQLLite and create rule dictionary
-			ruleDict=dict()
-			hour=int(india_time.strftime('%H'))
-			if hour>=2 and hour<6:
-			  daypart="1"
-			if hour>=6 and hour<10:
-			  daypart="2"
-			if hour>=10 and hour<14:
-			  daypart="3"
-			if hour>=14 and hour<18:
-			  daypart="4"
-			if hour>=18 and hour<22:
-			  daypart="5"
-			if hour>=22 and hour<2:
-			  daypart="6"
-			hour=str(hour)
-			weekday=india_time.strftime('%w')
-			if city=='':
-			  query = "SELECT * FROM rules WHERE (domain='"+domain+"' OR domain IS NULL) AND (city='"+city+"' OR city IS NULL) AND (state='"+state+"' OR state IS NULL) AND (weekday='"+weekday+"' OR weekday IS NULL) AND (hour='"+hour+"' OR hour IS NULL) AND (daypart='"+daypart+"' OR daypart IS NULL) AND (size='"+size+"' OR size IS NULL) AND (isp='"+isp+"' OR isp IS NULL) ORDER BY dimensions ASC"
+		    for ad in adSlots:
+			if segments:
+			    for seg in segments:
+				try:
+				    audienceCampaigns = campaignData['display:audience:' + str(seg)]
+				except KeyError:
+				    audienceCampaigns=list()
 			else:
-			  query = "SELECT * FROM rules WHERE (domain='"+domain+"' OR domain IS NULL) AND city IS NULL AND (state='"+state+"' OR state IS NULL) AND (weekday='"+weekday+"' OR weekday IS NULL) AND (hour='"+hour+"' OR hour IS NULL) AND (daypart='"+daypart+"' OR daypart IS NULL) AND (size='"+size+"' OR size IS NULL) AND (isp='"+isp+"' OR isp IS NULL) ORDER BY dimensions ASC"
-			cur.execute(query)
-			rows=cur.fetchall()
-			for row in rows:
-			  rules=json.loads(row[9])
-			  for key in rules.keys():
-			    ruleDict[key]=float(rules[key])
+			    audienceCampaigns=list()
 
-			#Loop over qualified campaigns and override the default bids with new bids from rules database
-			newCampList=[]
-			for camp in camplist:
-			  if str(camp[0]) in rulesDict.keys():
-			    camp[1]=float(rulesDict[str(camp[0])])
-			  newCampList.append(camp)
-			  
-			#Now start qualifying campaigns top-down by bids for pacing. If a campaign qualifies, choose it as a final candidate
-			finalCampaign=0
-			for camp in newCampList:
-			  r=random.randrange(1,100)
-			  if r<camp[2]:
-			    finalCampaign=camp[0]
-			    finalBid=camp[1]
-			    break
+			try:
+			    ronCampaigns = campaignData['display:roe']
+			except KeyError:
+			    ronCampaigns = list()
+
+			try:
+			    black = campaignData['display:roe:black:'+domain]		#ROE Campaigns with this domain as blacklist
+			except KeyError:
+			    black = list()
+
+			ronCampaigns = list(set(ronCampaigns) - set(black))			
+
+			try:
+			    whiteCampaigns = campaignData['display:white:'+domain]
+			except KeyError:
+			    whiteCampaigns = list()
+
+			campaigns = list(set(audienceCampaigns+ronCampaigns+whiteCampaigns))
+			
+			geoCampaigns = campaignData['display:geo:'+country]
+			campaigns = list(set(geoCampaigns) & set(campaigns))
+			
+			size=str(ad.width[0])+"x"+str(ad.height[0])
+			try:
+			    sizeCampaigns = campaignData['display:size:'+size]
+			except KeyError:
+			    sizeCampaigns = list()
 			    
-			if finalCampaign>0:    
-			    banners = campaignData['display:campaign:'+str(finalCampaign)+':'+str(ad.width[0])+'x'+str(ad.height[0])]
-			    randomBannerId = random.choice(banners)
-			    bidMicros = finalBid * 1000000
-			    info = base64.b64encode(json.dumps({'e':'GOOGLE','d':domain,'bid':randomBannerId,'cid':finalCampaign}))
-			    info = info.replace("+","-").replace("/","_").replace("=","")
-			    code='<iframe src="http://rtbidder.impulse01.com/serve?info='+info+'&p={WINNING_PRICE}&r={RANDOM}&red={CLICKURL}" width="'+str(ad.width[0])+'" height="'+str(ad.height[0])+'" frameborder=0 marginwidth=0 marginheight=0 scrolling=NO></iframe>'
-			    responsead = response.ad.add()
-			    responsead.html_snippet = code
-			    responsead.creative_id= randomBannerId
-			    responsead.click_through_url.append(campaignData['display:campaign:'+str(finalCampaign)+':url'])
-			    responseAdSlot = responsead.adslot.add()
-			    responseAdSlot.id=ad.id
-			    responseAdSlot.max_cpm_micros=int(bidMicros)
-		response.processing_time_ms=int((time.time()-start)*1000)
-	    else:
+			campaigns = list(set(sizeCampaigns) & set(campaigns))
+			
+			if(len(campaigns)>0):
+			    camplist=[]
+			    for camp in campaigns:
+				l = [camp, campaignData["display:campaign:"+str(camp)+":bid"],campaignData["display:campaign:"+str(camp)+":pacing"]]
+				camplist.append(l)
+			    camplist.sort(key=operator.itemgetter(1), reverse=True) # sorts the list in place decending by bids
+
+			    #Retrieve rules from SQLLite and create rule dictionary
+			    ruleDict=dict()
+			    hour=int(india_time.strftime('%H'))
+			    if hour>=2 and hour<6:
+				daypart="1"
+			    if hour>=6 and hour<10:
+				daypart="2"
+			    if hour>=10 and hour<14:
+				daypart="3"
+			    if hour>=14 and hour<18:
+				daypart="4"
+			    if hour>=18 and hour<22:
+				daypart="5"
+			    if hour>=22 and hour<2:
+				daypart="6"
+			    hour=str(hour)
+			    weekday=india_time.strftime('%w')
+			    if city=='':
+				query = "SELECT * FROM rules WHERE (domain='"+domain+"' OR domain IS NULL) AND (city='"+city+"' OR city IS NULL) AND (state='"+state+"' OR state IS NULL) AND (weekday='"+weekday+"' OR weekday IS NULL) AND (hour='"+hour+"' OR hour IS NULL) AND (daypart='"+daypart+"' OR daypart IS NULL) AND (size='"+size+"' OR size IS NULL) AND (isp='"+isp+"' OR isp IS NULL) ORDER BY dimensions ASC"
+			    else:
+				query = "SELECT * FROM rules WHERE (domain='"+domain+"' OR domain IS NULL) AND city IS NULL AND (state='"+state+"' OR state IS NULL) AND (weekday='"+weekday+"' OR weekday IS NULL) AND (hour='"+hour+"' OR hour IS NULL) AND (daypart='"+daypart+"' OR daypart IS NULL) AND (size='"+size+"' OR size IS NULL) AND (isp='"+isp+"' OR isp IS NULL) ORDER BY dimensions ASC"
+			    cur.execute(query)
+			    rows=cur.fetchall()
+			    for row in rows:
+				rules=json.loads(row[9])
+				for key in rules.keys():
+				    ruleDict[key]=float(rules[key])
+
+			    #Loop over qualified campaigns and override the default bids with new bids from rules database
+			    newCampList=[]
+			    for camp in camplist:
+				if str(camp[0]) in rulesDict.keys():
+				    camp[1]=float(rulesDict[str(camp[0])])
+				newCampList.append(camp)
+			      
+			    #Now start qualifying campaigns top-down by bids for pacing. If a campaign qualifies, choose it as a final candidate
+			    finalCampaign=0
+			    for camp in newCampList:
+				r=random.randrange(1,100)
+				if r<camp[2]:
+				    finalCampaign=camp[0]
+				    finalBid=camp[1]
+				    break
+				
+			    if finalCampaign>0:    
+				banners = campaignData['display:campaign:'+str(finalCampaign)+':'+str(ad.width[0])+'x'+str(ad.height[0])]
+				randomBannerId = random.choice(banners)
+				bidMicros = finalBid * 1000000
+				info = base64.b64encode(json.dumps({'e':'GOOGLE','d':domain,'bid':randomBannerId,'cid':finalCampaign}))
+				info = info.replace("+","-").replace("/","_").replace("=","")
+				code='<iframe src="http://rtbidder.impulse01.com/serve?info='+info+'&p={WINNING_PRICE}&r={RANDOM}&red={CLICKURL}" width="'+str(ad.width[0])+'" height="'+str(ad.height[0])+'" frameborder=0 marginwidth=0 marginheight=0 scrolling=NO></iframe>'
+				responsead = response.ad.add()
+				responsead.html_snippet = code
+				responsead.creative_id= randomBannerId
+				responsead.click_through_url.append(campaignData['display:campaign:'+str(finalCampaign)+':url'])
+				responseAdSlot = responsead.adslot.add()
+				responseAdSlot.id=ad.id
+				responseAdSlot.max_cpm_micros=int(bidMicros)
+		    response.processing_time_ms=int((time.time()-start)*1000)
+		else:
+		    response = realtime_bidding_proto_pb2.BidResponse()
+		    response.processing_time_ms=int((time.time()-start)*1000)
+		    
+		bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"] += 1
+		if int(time.time() - bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Lastupdate"])>10:
+		  i = bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"]
+		  message = json.dumps({"messageType":"Forecast", "message":{"e":"GoogleAdX", "d":domain , "c":"DesktopDisplay" ,"geo":country.upper(),
+					  "size":str(ad.width[0])+'x'+str(ad.height[0]) , "i":i}})
+		  bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"]=0
+		  bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Lastupdate"]=time.time()
+		  sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+		  sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+		  sock.sendto(message, (UDP_IP, UDP_PORT))
+		
+	    except:
 		response = realtime_bidding_proto_pb2.BidResponse()
 		response.processing_time_ms=int((time.time()-start)*1000)
-		
-	    bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"] += 1
-	    if int(time.time() - bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Lastupdate"])>10:
-	      i = bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"]
-	      message = json.dumps({"messageType":"Forecast", "message":{"e":"GoogleAdX", "d":domain , "c":"DesktopDisplay" ,"geo":country.upper(),
-				      "size":str(ad.width[0])+'x'+str(ad.height[0]) , "i":i}})
-	      bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"]=0
-	      bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Lastupdate"]=time.time()
-	      sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-	      sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-	      sock.sendto(message, (UDP_IP, UDP_PORT))
-	      print "sending inventory update package"
+		traceback.print_exc(file=sys.stdout)
+
+	    responseString = response.SerializeToString()
+	    self.write(responseString)
+	    self.finish()
+    
+	if self.request.path == "/result":
+	    self.write("Nop")
 	    
-	except:
-	    response = realtime_bidding_proto_pb2.BidResponse()
-	    response.processing_time_ms=int((time.time()-start)*1000)
-	    traceback.print_exc(file=sys.stdout)
-
-        responseString = response.SerializeToString()
-	self.write(responseString)
-	self.finish()
-
 def autovivify(levels=1, final=dict):
     return (defaultdict(final) if levels < 2 else defaultdict(lambda: autovivify(levels - 1, final)))
 
@@ -242,8 +248,8 @@ def refreshRules():
     for key in rulesIndex.keys():
 	sm=key.split("|")
 	for n,i in enumerate(sm):
-	  if i=='*':
-	    sm[n]=None
+	    if i=='*':
+		sm[n]=None
 	record = (sm[0],sm[1],sm[2],sm[3],sm[4],sm[5],sm[6],sm[7],(8-key.count("*")),json.dumps(rulesIndex[key]))
 	queryData.append(record)
     cur.execute("DELETE FROM rules")
@@ -273,7 +279,7 @@ geoIndex=dict()
 location = open("location.csv","r").read()
 reader = csv.reader(location.split('\n'), delimiter=',')
 for row in reader:
-  geoIndex[row[0]]={"Name":row[1], "Parent":row[3], "Type":row[5],"Country":row[4]}
+    geoIndex[row[0]]={"Name":row[1], "Parent":row[3], "Type":row[5],"Country":row[4]}
 print options.name+" Loaded geoIndex from location.csv"
 del location
 del reader
@@ -316,8 +322,8 @@ queryData=[]
 for key in rulesIndex.keys():
     sm=key.split("|")
     for n,i in enumerate(sm):
-      if i=='*':
-        sm[n]=None
+	if i=='*':
+	    sm[n]=None
     record = (sm[0],sm[1],sm[2],sm[3],sm[4],sm[5],sm[6],sm[7],(8-key.count("*")),json.dumps(rulesIndex[key]))
     queryData.append(record)
 cur.executemany('INSERT INTO rules VALUES (?,?,?,?,?,?,?,?,?,?)', queryData)    
