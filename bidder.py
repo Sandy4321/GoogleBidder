9b8dbb44a18114ec04d12756432aa723923bc679
Aditya Singh <aditya@impulsemedia.co.in>
2013-01-03 13:26:11 +0530

    modified:   bidder.py

--- bidder.py
+++ bidder.py
@@ -222,20 +222,19 @@ class MainHandler(tornado.web.RequestHandler):
 				responseAdSlot.id=ad.id
 				responseAdSlot.max_cpm_micros=int(bidMicros)
 		    response.processing_time_ms=int((time.time()-start)*1000)
+		    bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"] += 1
+		    if int(time.time() - bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Lastupdate"])>10:
+		      i = bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"]
+		      message = json.dumps({"messageType":"Forecast", "message":{"e":"GoogleAdX", "d":domain , "c":"DesktopDisplay" ,"geo":country.upper(),
+					      "size":str(ad.width[0])+'x'+str(ad.height[0]) , "i":i}})
+		      bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"]=0
+		      bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Lastupdate"]=time.time()
+		      sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+		      sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+		      sock.sendto(message, (UDP_IP, UDP_PORT))		    
 		else:
 		    response = realtime_bidding_proto_pb2.BidResponse()
 		    response.processing_time_ms=int((time.time()-start)*1000)
-		    
-		bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"] += 1
-		if int(time.time() - bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Lastupdate"])>10:
-		  i = bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"]
-		  message = json.dumps({"messageType":"Forecast", "message":{"e":"GoogleAdX", "d":domain , "c":"DesktopDisplay" ,"geo":country.upper(),
-					  "size":str(ad.width[0])+'x'+str(ad.height[0]) , "i":i}})
-		  bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Impressions"]=0
-		  bidCountIndex["GoogleAdX"][domain]["DesktopDisplay"][country.upper()][str(ad.width[0])+'x'+str(ad.height[0])]["Lastupdate"]=time.time()
-		  sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-		  sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-		  sock.sendto(message, (UDP_IP, UDP_PORT))
 		
 	    except:
 		response = realtime_bidding_proto_pb2.BidResponse()
