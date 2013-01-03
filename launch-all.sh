450e4c3154677532f7fda12cfeb905e0f172bdd2
Aditya Singh <aditya@impulsemedia.co.in>
2013-01-03 15:26:24 +0530

    modified:   launch-all.sh 	modified:   nginx.conf

--- launch-all.sh
+++ launch-all.sh
@@ -2,3 +2,7 @@ PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp python bidder.py -port=8888 -name=Tor
 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp python bidder.py -port=8889 -name=Tornado2 -refreshCache=35000 -rulesRefresh=1020000&
 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp python bidder.py -port=8890 -name=Tornado3 -refreshCache=25000 -rulesRefresh=1030000&
 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp python bidder.py -port=8891 -name=Tornado4 -refreshCache=30000 -rulesRefresh=1040000&
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp python bidder.py -port=8892 -name=Tornado5 -refreshCache=30000 -rulesRefresh=1040000&
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp python bidder.py -port=8893 -name=Tornado6 -refreshCache=30000 -rulesRefresh=1040000&
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp python bidder.py -port=8894 -name=Tornado7 -refreshCache=30000 -rulesRefresh=1040000&
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp python bidder.py -port=8895 -name=Tornado8 -refreshCache=30000 -rulesRefresh=1040000&
